5ead297c2d6bc54504936eaf146667b7704f24b5
irma660 <1325455@senati.pe>
2023-10-07 19:40:59 -0500

    ajustando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>CITY TOURS</title>
 </head>
 <body>
-<div class="centrar-contenido">
+
 <h1> Bienvenido a City Tours </h1> 
-<img src="{{ asset('img/logo.png') }}" alt="Descripción de la imagen">
+<div class="centrar-contenido">
+<img src="{{ asset('img/logo.jpg') }}" width="231" height="126">
 </div>
      
 </body>
