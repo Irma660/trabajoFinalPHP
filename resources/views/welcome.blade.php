8a7392bf3483c3487da6469f069f4a38cb22b1c0
irma660 <1325455@senati.pe>
2023-10-08 16:55:29 -0500

    add descripción

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
     
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
     <link rel="icon"  href="iconos/mon.ico" />
     <title>CITY TOURS</title>
@@ -16,25 +19,31 @@
 <img src="{{ asset('img/logo.jpg') }}" width="331" height="226">
 </div>
 <div class="limit">
-            <h2>Conoce los mejores lugares turísticos de Huánuco</h2>
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<h2 class="font-weight-bold" style="font-family: 'Roboto', sans-serif;">Conoce los mejores lugares turísticos de Huánuco</h2>
+            
             <div class="carousel-container-wrapper">
             <div class="carousel-container">
         <div class="carousel">
             <div class="carousel-slide">
                 <img src="{{ asset('img/huanuco.jpg') }}" alt="Imagen 1">
+                <div class="image-description">HUÁNUCO</div>
             </div>
             <div class="carousel-slide">
                 <img src="{{ asset('img/dosdemayo.jpg') }}" alt="Imagen 2">
+                <div class="image-description">DOS DE MAYO</div>
             </div>
             <div class="carousel-slide">
                 <img src="{{ asset('img/tomayquicha.jpg') }}" alt="Imagen 3">
+                <div class="image-description">TOMAYKICHWA</div>
             </div>
             <div class="carousel-slide">
                 <img src="{{ asset('img/garu.jpg') }}" alt="Imagen 4">
+                <div class="image-description">COMPLEJO ARQUEOLÓGICO GARU</div>
             </div>
             <div class="carousel-slide">
                 <img src="{{ asset('img/parquetingo.jpg') }}" alt="Imagen 5">
+                <div class="image-description">PARQUE NACIONAL DE TINGO MARÍA</div>
             </div>
         </div>
         <button id="prevBtn">Anterior</button>
