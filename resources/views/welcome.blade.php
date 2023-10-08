a7eafa74f71a332da59c7b0f8c7d476a2a52597b
irma660 <1325455@senati.pe>
2023-10-07 20:27:20 -0500

    añadiendo icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon"  href="iconos/mon.ico" />
     <title>CITY TOURS</title>
 </head>
 <body>
